17fc98ad234ed683c44041da9e7ef0510ad6cc13
maik656 <maik656@gmail.com>
2012-12-29 21:54:51 +0100

    afegida una comprovació al onDestroy per a acabar el discovery si potser l'estava fent mentre es tanca l'aplicació

--- src/fib/pec/hovione/MainActivity.java
+++ src/fib/pec/hovione/MainActivity.java
@@ -121,7 +121,7 @@ public class MainActivity extends FragmentActivity {
     protected void onDestroy() {
         super.onDestroy();
         //this.unregisterReceiver(mReceiver);
-        bluetoothManager.endDiscovery();
+        if (bluetoothManager != null) bluetoothManager.endDiscovery(); 
     }
 	
 	@Override
